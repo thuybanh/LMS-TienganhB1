cd0bdd8336c52456b309d5ad2c5301b006df7a98
Thuy <kakaka1549@gmail.com>
2026-09-25 22:26:30 +0700

    Initial version

---
+++ script.js
@@ -0,0 +1,217 @@
+const lessons = {
+
+    1: [
+
+        {
+            word: "abandon",
+            type: "v",
+            ipa: "/əˈbændən/",
+            meaning: "bỏ rơi"
+        },
+
+        {
+            word: "abuse",
+            type: "n",
+            ipa: "/əˈbjuːs/",
+            meaning: "sự lạm dụng"
+        },
+
+        {
+            word: "analyze",
+            type: "v",
+            ipa: "/ˈænəlaɪz/",
+            meaning: "phân tích"
+        },
+
+        {
+            word: "benefit",
+            type: "v",
+            ipa: "/ˈbenɪfɪt/",
+            meaning: "hưởng lợi"
+        },
+
+        {
+            word: "communicate",
+            type: "v",
+            ipa: "/kəˈmjuːnɪkeɪt/",
+            meaning: "giao tiếp"
+        },
+
+        {
+            word: "conditions",
+            type: "n",
+            ipa: "/kənˈdɪʃənz/",
+            meaning: "điều kiện"
+        },
+
+        {
+            word: "conservation",
+            type: "n",
+            ipa: "/ˌkɒnsəˈveɪʃən/",
+            meaning: "sự bảo tồn"
+        },
+
+        {
+            word: "convince",
+            type: "v",
+            ipa: "/kənˈvɪns/",
+            meaning: "thuyết phục"
+        },
+
+        {
+            word: "cruel",
+            type: "adj",
+            ipa: "/ˈkruːəl/",
+            meaning: "tàn nhẫn"
+        },
+
+        {
+            word: "debate",
+            type: "v",
+            ipa: "/dɪˈbeɪt/",
+            meaning: "tranh luận"
+        },
+
+        {
+            word: "domestic",
+            type: "adj",
+            ipa: "/dəˈmestɪk/",
+            meaning: "nội địa"
+        },
+
+        {
+            word: "emergency",
+            type: "n",
+            ipa: "/ɪˈmɜːdʒənsi/",
+            meaning: "khẩn cấp"
+        },
+
+        {
+            word: "environment",
+            type: "n",
+            ipa: "/ɪnˈvaɪərənmənt/",
+            meaning: "môi trường"
+        },
+
+        {
+            word: "feed",
+            type: "v",
+            ipa: "/fiːd/",
+            meaning: "cho ăn"
+        },
+
+        {
+            word: "harmless",
+            type: "adj",
+            ipa: "/ˈhɑːmləs/",
+            meaning: "vô hại"
+        },
+
+        {
+            word: "herd",
+            type: "n",
+            ipa: "/hɜːd/",
+            meaning: "bầy đàn"
+        },
+
+        {
+            word: "humane",
+            type: "adj",
+            ipa: "/hjuːˈmeɪn/",
+            meaning: "nhân đạo, nhân văn"
+        },
+
+        {
+            word: "involve",
+            type: "v",
+            ipa: "/ɪnˈvɒlv/",
+            meaning: "liên quan đến"
+        },
+
+        {
+            word: "issue",
+            type: "n",
+            ipa: "/ˈɪʃuː/",
+            meaning: "vấn đề"
+        },
+
+        {
+            word: "poisonous",
+            type: "adj",
+            ipa: "/ˈpɔɪzənəs/",
+            meaning: "độc hại, có độc"
+        },
+
+        {
+            word: "protect",
+            type: "v",
+            ipa: "/prəˈtekt/",
+            meaning: "bảo vệ"
+        },
+
+        {
+            word: "realize",
+            type: "v",
+            ipa: "/ˈrɪəlaɪz/",
+            meaning: "nhận ra"
+        },
+
+        {
+            word: "result",
+            type: "n",
+            ipa: "/rɪˈzʌlt/",
+            meaning: "kết quả"
+        },
+
+        {
+            word: "savannah",
+            type: "n",
+            ipa: "/səˈvænə/",
+            meaning: "thảo nguyên"
+        },
+
+        {
+            word: "search for",
+            type: "v",
+            ipa: "/sɜːtʃ fɔː/",
+            meaning: "tìm kiếm"
+        },
+
+        {
+            word: "sedate",
+            type: "v",
+            ipa: "/sɪˈdeɪt/",
+            meaning: "cho dùng thuốc an thần"
+        },
+
+        {
+            word: "specialization",
+            type: "n",
+            ipa: "/ˌspeʃəlaɪˈzeɪʃən/",
+            meaning: "chuyên môn hóa"
+        },
+
+        {
+            word: "suffer",
+            type: "v",
+            ipa: "/ˈsʌfə/",
+            meaning: "chịu đựng"
+        },
+
+        {
+            word: "survive",
+            type: "v",
+            ipa: "/səˈvaɪv/",
+            meaning: "tồn tại"
+        },
+
+        {
+            word: "zoology",
+            type: "n",
+            ipa: "/zuˈɒlədʒi/",
+            meaning: "động vật học"
+        }
+
+    ]
+
+};
